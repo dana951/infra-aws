84d60f24bbd7a309c658da750e724966a3573697
dana951 <danar@example.com>
2026-04-20 18:52:29 +0300

    feat: bootstrap Argo CD main app on EKS

--- accounts/devops/eks-tools/data.tf
+++ accounts/devops/eks-tools/data.tf
@@ -9,3 +9,7 @@ data "terraform_remote_state" "eks_cluster" {
 data "aws_eks_cluster_auth" "eks_cluster" {
   name = data.terraform_remote_state.eks_cluster.outputs.cluster_name
 }
+
+data "http" "argocd_main_app_yaml" {
+  url = "https://raw.githubusercontent.com/dana951/argocd-apps/main/main-app.yaml"
+}
